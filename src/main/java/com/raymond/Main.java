a4a6e79b8d9539becf817cec9f181ef8360cf492
raymond <raymond8825@foxmail.com>
2021-10-20 22:24:01 +0800

    完成逐行读取

---
+++ src/main/java/com/raymond/Main.java
@@ -0,0 +1,29 @@
+package com.raymond;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        String basePath = "C:\\Users\\PeterLei\\Desktop";
+        File file = new File(basePath);
+        String[] list = file.list();
+        ArrayList<String> needToConvertList = new ArrayList<>();
+        for (String s : list) {
+            if (s.endsWith(".md")) {
+                needToConvertList.add(s);
+            }
+        }
+        String temp = basePath + File.separator + needToConvertList.get(0);
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(temp)))) {
+            String readLine = null;
+            while ((readLine = bufferedReader.readLine()) != null) {
+                if (readLine.startsWith("![](")){
+                    System.out.println(readLine);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
